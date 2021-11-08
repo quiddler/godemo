917582aa82b9d951c0f8628752d35dd6cb84d593
Eliot Easterling <eeasterling@dakotasoft.com>
2021-11-08 16:35:21 -0500

    refactoring

--- go.mod
+++ go.mod
@@ -3,5 +3,5 @@ module github.com/quiddler/godemo
 go 1.17
 
 require (
-	github.com/githubnemo/CompileDaemon
+	
 )
--- main.go
+++ main.go
@@ -3,18 +3,33 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/quiddler/godemo/homepg"
+	"github.com/quiddler/godemo/server"
 )
 
-const message = "Hello fellow gophers!"
+// run the following command to get the .pem and .crt files that tls relies on:
+// openssl req -x509 -nodes -newkey ec -pkeyopt ec_paramgen_curve:secp384r1 -keyout ecdsa.pem -out mycert.crt -days 3650
+var (
+	KeyFile     = "./certs/ecdsa.pem"
+	CertFile    = "./certs/mycert.crt"
+	ServiceAddr = ":8080"
+)
 
 func main() {
+
+	home := homepg.HomePage{
+		Logger: log.New(os.Stdout, "demo - ", log.LstdFlags|log.Lshortfile),
+	}
+
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text-plain; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(message))
-	})
-	err := http.ListenAndServe(":8080", mux)
+
+	mux.HandleFunc("/", home.Handler)
+
+	s := server.New(mux, ServiceAddr)
+
+	err := s.ListenAndServeTLS(CertFile, KeyFile)
 	if err != nil {
 		log.Fatalf("server failed to start: %v", err)
 	}
